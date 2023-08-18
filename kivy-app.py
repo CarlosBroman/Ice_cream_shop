34fb9ef8af6b47f8526b8a80e7602dd1a2f3daaf
Carlos Román <carlos.roman.baena@gmail.com>
2023-08-18 02:13:26 +0200

    Dropdown button fails from time to time

--- kivy-app.py
+++ kivy-app.py
@@ -12,19 +12,17 @@ import pandas as pd
 import datetime
 
 CSV_FILE_NAME = "ice_cream_data.csv"
-NEW_FLAVOR_OPERATION = "Nuevo sabor"
-EMPTY_STOCK_THRESHOLD = 1
-
-existing_df = pd.read_csv(CSV_FILE_NAME)
+EMPTY_STOCK_THRESHOLD = 2
 
 class icecream(App):
     
 # CREATE LAYOUT OF INITIAL SCREEN    
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
+        self.existing_df = pd.read_csv(CSV_FILE_NAME)
         self.selected_helado = None
         self.selected_quantity = None
-        self.selected_operacion = None
+        self.selected_operation = None
         self.helado_text_input = None
     
     def build(self):
@@ -47,8 +45,8 @@ class icecream(App):
 
     def add_buttons(self, layout):
         buttons = [
-            ("Generar movimiento", self.generate_movement),
-            ("Añadir nuevo sabor", self.add_new_flavor),
+            ("Entrada / salida helado existente", lambda _: self.show_movement_popup()),
+            ("Añadir nuevo sabor", lambda _: self.add_new_flavour_popup()),
             ("Stocks", self.show_stocks)
         ]
         for button_text, callback in buttons:
@@ -58,197 +56,162 @@ class icecream(App):
 
 # ADD FUNCTIONALITY TO BUTTONS
 
-# GENERATE MOVEMENT BUTTON
-    def generate_movement(self, instance):
-        self.show_movement_popup()
+    def generate_popup(self, title, content_widgets, num_columns):
+        popup_content = GridLayout(cols=num_columns)
+        for widget in content_widgets:
+            popup_content.add_widget(widget)
+            
+        popup = Popup(title=title, content=popup_content, size_hint=(None, None), size=(400, 400))
         
+        cancel_button = Button(text="Cerrar", size_hint=(None, None), size=(100, 40))
+        cancel_button.bind(on_release=lambda _: popup.dismiss())
+        popup_content.add_widget(cancel_button)
+        
+        
+        popup.open()
+    
+    def message_popup(self, message):
+        title = "Movimiento"
+        content_widgets = [
+            Label(text=message),
+        ]
+        num_columns = 1
+        self.generate_popup(title, content_widgets, num_columns)
+
+    def update_button_and_close_dropdown(self, dropdown, button, selected_value, selected_value_attr):
+        button.text = selected_value
+        
+        if selected_value_attr == "selected_helado":
+             self.selected_helado = selected_value
+        elif selected_value_attr == "selected_quantity":
+            self.selected_quantity = selected_value
+        elif selected_value_attr == "selected_operation":
+            self.selected_operation = selected_value
+        
+        dropdown.dismiss()
+
+    def create_new_entry(self, helado, cantidad, operacion, stock, message):
+            now = datetime.datetime.now()
+            id = len(self.existing_df["ID"].tolist())
+                    
+            new_entry = {
+                "ID": id,
+                "Helado": helado,
+                "Cantidad": cantidad,
+                "Operacion": operacion,
+                "Stock": stock,
+                "Fecha": now.strftime("%Y-%m-%d"),
+                "Hora": now.strftime("%H:%M:%S"),
+                "Hora numero": now.hour * 3600 + now.minute * 60 + now.second
+            }
+            
+            self.existing_df = pd.concat([self.existing_df, pd.DataFrame([new_entry])])
+            self.existing_df.to_csv(CSV_FILE_NAME, index=False)
+            self.message_popup(message)
+
+# GENERATE MOVEMENT BUTTON       
     def show_movement_popup(self):
-        popup_content = GridLayout(cols=1, pos_hint={'center_x': 0.5, 'center_y': 0.5})
-        
-        helado_dropdown = DropDown()
-        unique_helados = existing_df["Helado"].unique()
-        for helado in unique_helados:
-            btn = Button(text=helado, size_hint_y=None, height=44)
-            btn.bind(on_release=lambda btn: self.update_button_and_close_dropdown(self.helado_button, helado_dropdown, btn.text))
-            helado_dropdown.add_widget(btn)
-            helado_dropdown.bind(on_select=self.update_button_and_close_dropdown)
-            
-        self.helado_button = Button(text="Select Helado", size_hint=(None, None), size=(150, 50))
-        self.helado_button.bind(on_release=helado_dropdown.open)
-        popup_content.add_widget(self.helado_button)
+        title = "New movement"
+        content_widgets = [
+            self.create_helado_dropdown(),
+            self.create_quantity_dropdown(),
+            self.create_operation_dropdown(),
+            Label(size_hint_y=None, height=30),
+            Button(text="Generate", size_hint=(None, None), size=(100, 50), on_release=self.generate_entry),
+        ]
+        num_columns = 1
+        self.generate_popup(title, content_widgets, num_columns)
 
-        quantity_dropdown = DropDown()
-        for quantity in range(1, 10):
-            btn = Button(text=str(quantity), size_hint_y=None, height=44)
-            btn.bind(on_release=lambda btn: self.update_button_and_close_dropdown(self.quantity_button, quantity_dropdown, btn.text))
-            quantity_dropdown.add_widget(btn)
-            
-        self.quantity_button = Button(text="Select Quantity", size_hint=(None, None), size=(150, 50))
-        self.quantity_button.bind(on_release=quantity_dropdown.open)
-        popup_content.add_widget(self.quantity_button)
+    def create_dropdown(self, values, button_text, selected_value_attr):
+        dropdown = DropDown()
+        text = button_text
+        button = Button(text=text, size_hint=(None, None), size=(150, 50), on_release=dropdown.open)
+        
+        for value in values:
+            btn = Button(text=value, size_hint_y=None, height=44)
+            btn.bind(on_release=lambda btn: self.update_button_and_close_dropdown(dropdown, button, btn.text, selected_value_attr))
+            dropdown.add_widget(btn)
 
-        operation_dropdown = DropDown()
-        for operation in ["Entrada", "Salida"]:
-            btn = Button(text=operation, size_hint_y=None, height=44)
-            btn.bind(on_release=lambda btn: self.update_button_and_close_dropdown(self.operation_button, operation_dropdown, btn.text))
-            operation_dropdown.add_widget(btn)
-            
-        self.operation_button = Button(text="Select Operation", size_hint=(None, None), size=(150, 50))
-        self.operation_button.bind(on_release=operation_dropdown.open)
-        popup_content.add_widget(self.operation_button)
-        
-        empty_space = Label(size_hint_y=None, height=30)
-        popup_content.add_widget(empty_space)
-        
-        generate_button = Button(text="Generate", size_hint=(None, None), size=(100, 50))
-        generate_button.bind(on_release=self.generate_entry)
-        popup_content.add_widget(generate_button)
-        
-        close_button = Button(text="Close", size_hint=(None, None), size=(100, 50))
-        close_button.bind(on_release=self.dismiss_popup)
-        popup_content.add_widget(close_button)
-        
-        self.popup = Popup(title="New movement", content=popup_content, size_hint=(None, None), size=(300, 350))
-        self.popup.open()
+        setattr(self, selected_value_attr, button.text)
+        return button
+
+    def create_helado_dropdown(self):
+        x = self.existing_df["Helado"].unique()
+        button_name = "Sabor de helado"
+        attribute = "selected_helado"
+        return self.create_dropdown(x, button_name, attribute)
+
+    def create_quantity_dropdown(self):
+        x = [str(number) for number in range(1, 10)]
+        button_name = "Cantidad"
+        selected_quantity = "selected_quantity"
+        return self.create_dropdown(x,  button_name, selected_quantity)
+
+    def create_operation_dropdown(self):
+        x = ["Entrada", "Salida"]
+        button_name = "Entrada o salida"
+        selected_operation = "selected_operation"
+        return self.create_dropdown(x,  button_name, selected_operation)
 
     def generate_entry(self, instance):
         # Functionality to generate entry based on selected values
-        global existing_df
-        now = datetime.datetime.now()
-        id = len(existing_df["ID"].tolist())
         helado = self.selected_helado
-        cantidad = self.selected_quantity
-        operacion = self.selected_operacion
-        stock = existing_df[existing_df["Helado"] == helado]["Stock"].iloc[-1]
+        cantidad = int(self.selected_quantity)
+        operacion = self.selected_operation
+        stock = self.existing_df[self.existing_df["Helado"] == helado]["Stock"].iloc[-1]
         
         if operacion == "Entrada":
             stock = stock + cantidad
         elif operacion == "Salida":
             stock = stock - cantidad
-                
-        new_entry = {
-            "ID": id,
-            "Helado": helado,
-            "Cantidad": cantidad,
-            "Operacion": operacion,
-            "Stock": stock,
-            "Fecha": now.strftime("%Y-%m-%d"),
-            "Hora": now.strftime("%H:%M:%S"),
-            "Hora numero": now.hour * 3600 + now.minute * 60 + now.second
-        }
+            
+        message = f"Se ha hecho una {operacion} de {cantidad} unidades de {helado}"
+        self.create_new_entry(helado, cantidad, operacion, stock, message)
         
-        existing_df = icecream.save_to_csv(pd.concat([existing_df, pd.DataFrame([new_entry])]))
-        message = f"Se ha hecho una {operacion.lower()} de {cantidad} de {helado}"
-        self.show_message_popup("Movimiento", message)
-
-    def show_message_popup(self, title, message):
-        popup_content = GridLayout(cols = 1)
-        popup_content.add_widget(Label(text=message))
-        
-        close_button = Button(text="Close", size_hint=(None, None), size=(100, 50))
-        close_button.bind(on_release=self.dismiss_message_popup)
-        popup_content.add_widget(close_button)
-        
-        self.message_popup = Popup(title=title, content=popup_content, size_hint=(None, None), size=(300, 200))
-        self.message_popup.open()
-        
-    def dismiss_message_popup(self, instance):
-        self.message_popup.dismiss()
-        
-    def update_button_and_close_dropdown(self, button, dropdown, new_text):
-        self.update_button_text(button, new_text)
-        dropdown.dismiss()
-        
-        if button is self.helado_button:
-            self.selected_helado = new_text
-        elif button is self.quantity_button:
-            self.selected_quantity = int(new_text)
-        elif button is self.operation_button:
-            self.selected_operacion = new_text
-        
-    def update_button_text(self, button, new_text):
-       button.text = new_text
-
-# ADD FLAVOUR BUTTON
-  
-    def add_new_flavor(self, instance):
-        self.add_new_flavour_popup()
-        
+# ADD FLAVOUR BUTTON   
     def add_new_flavour_popup(self):
-        popup_content = GridLayout(cols = 1)
-        self.helado_text_input = TextInput(hint_text="Enter Ice Cream Flavor", size_hint=(None, None), size=(150, 30))
-        popup_content.add_widget(self.helado_text_input)
-        
+        helado_text_input = TextInput(hint_text="Enter Ice Cream Flavor", size_hint=(None, None), size=(150, 30))
         add_button = Button(text="Add Flavor", size_hint=(None, None), size=(150, 40))
-        add_button.bind(on_release=self.add_the_flavour)  # Bind to the function that adds the flavor
-        popup_content.add_widget(add_button)
-                
-        cancel_button = Button(text="Cancel", size_hint=(None, None), size=(100, 40))
-        cancel_button.bind(on_release=self.dismiss_popup)  # Bind to dismiss the popup
-        popup_content.add_widget(cancel_button)
+        add_button.bind(on_release=lambda _: self.add_new_flavour(helado_text_input))
         
-        self.popup = Popup(title="Add New Flavor", content=popup_content, size_hint=(None, None), size=(300, 200))
-        self.popup.open()
+        title = "Añadir nuevo sabor"
+        content_widgets = [helado_text_input, add_button]
+        num_columns=1
+        self.generate_popup(title, content_widgets, num_columns) 
         
-    def add_the_flavour(self, instance):
-        global existing_df
-        now = datetime.datetime.now()
-        id = len(existing_df["ID"].tolist())
-        helado = self.helado_text_input.text
+    def add_new_flavour(self, helado_text_input):
+        helado = helado_text_input.text
         cantidad = 0
         operacion = "Nuevo sabor"
         stock = 0
-                
-        new_entry = {
-            "ID": id,
-            "Helado": helado,
-            "Cantidad": cantidad,
-            "Operacion": operacion,
-            "Stock": stock,
-            "Fecha": now.strftime("%Y-%m-%d"),
-            "Hora": now.strftime("%H:%M:%S"),
-            "Hora numero": now.hour * 3600 + now.minute * 60 + now.second
-        }
-        
-        existing_df = icecream.save_to_csv(pd.concat([existing_df, pd.DataFrame([new_entry])]))
         message = f"Se ha introducido {helado} a la base de datos"
-        self.show_message_popup("Movimiento", message)
+        
+        self.create_new_entry(helado, cantidad, operacion, stock, message)
 
+# SHOW STOCKS
+    def show_stock_popup(self, stock_info, restock_info):
+        title = "Stocks"
+        content_widgets = [
+            Label(text=stock_info),
+            Label(text=restock_info)
+        ]
+        num_columns = 2
+        self.generate_popup(title, content_widgets, num_columns)
+        
     def show_stocks(self, instance):
         stock_info = "STOCK\n\n"
         restock_info ="EMPTY STOCK\n\n"
-        unique_helados = existing_df['Helado'].unique()
+        
+        unique_helados = self.existing_df['Helado'].unique()
 
         for helado in unique_helados:
-            last_stock = existing_df[existing_df["Helado"] == helado]["Stock"].iloc[-1]
-            if last_stock >1:
+            last_stock = self.existing_df[self.existing_df["Helado"] == helado]["Stock"].iloc[-1]
+            if last_stock > EMPTY_STOCK_THRESHOLD:
                 stock_info += f"{helado}: {last_stock}\n"
             else:
-                restock_info += f"{helado}: {last_stock}\n"
-        
-        
+                restock_info += f"{helado}: {last_stock}\n"        
         
         self.show_stock_popup(stock_info, restock_info)
         
-    def show_stock_popup(self, stock_info, restock_info):
-        popup_content = GridLayout(cols = 2)
-        popup_content.add_widget(Label(text=stock_info))
-        popup_content.add_widget(Label(text=restock_info))
-        close_button = Button(text="Close", size_hint=(None, None), size=(100, 50))
-        close_button.bind(on_release=self.dismiss_popup)
-        popup_content.add_widget(close_button)
-        
-        self.popup = Popup(title="Stocks", content=popup_content, size_hint=(None, None), size=(400, 400))
-        self.popup.open()
-        
-    def dismiss_popup(self, instance):
-        self.popup.dismiss()
-    
-    @staticmethod
-    def save_to_csv(dataframe):
-        CSV_FILE_NAME = "ice_cream_data.csv"
-        dataframe.to_csv(CSV_FILE_NAME, index=False)
-        return dataframe
-
 app = icecream()
 app.run()
--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,19 +1,91 @@
-  self.add_widget(self.s_name)
+   def update_button_and_close_dropdown(self, dropdown, button, selected_value, selected_value_attr):
+        button.text = selected_value
         
-#         self.add_widget(Label(text = 'Student Marks'))
-#         self.s_marks = TextInput()
-#         self.add_widget(self.s_marks)
+        if selected_value_attr == "selected_helado":
+             self.selected_helado = selected_value
+        elif selected_value_attr == "selected_quantity":
+            self.selected_quantity = selected_value
+        elif selected_value_attr == "selected_operation":
+            self.selected_operation = selected_value
         
-#         self.add_widget(Label(text = 'Student Gender'))
-#         self.s_gender = TextInput()
-#         self.add_widget(self.s_gender)
+        dropdown.dismiss()
+
+    def create_new_entry(self, helado, cantidad, operacion, stock, message):
+            global existing_df
+            now = datetime.datetime.now()
+            id = len(existing_df["ID"].tolist())
+                    
+            new_entry = {
+                "ID": id,
+                "Helado": helado,
+                "Cantidad": cantidad,
+                "Operacion": operacion,
+                "Stock": stock,
+                "Fecha": now.strftime("%Y-%m-%d"),
+                "Hora": now.strftime("%H:%M:%S"),
+                "Hora numero": now.hour * 3600 + now.minute * 60 + now.second
+            }
+            
+            existing_df = pd.concat([existing_df, pd.DataFrame([new_entry])])
+            existing_df.to_csv(CSV_FILE_NAME, index=False)
+            self.message_popup(message)
+
+# GENERATE MOVEMENT BUTTON       
+    def show_movement_popup(self):
+        title = "New movement"
+        content_widgets = [
+            self.create_helado_dropdown(),
+            self.create_quantity_dropdown(),
+            self.create_operation_dropdown(),
+            Label(size_hint_y=None, height=30),
+            Button(text="Generate", size_hint=(None, None), size=(100, 50), on_release=self.generate_entry),
+        ]
+        num_columns = 1
+        self.generate_popup(title, content_widgets, num_columns)
+
+    def create_dropdown(self, values, button_text, selected_value_attr):
+        dropdown = DropDown()
+        text = button_text
+        button = Button(text=text, size_hint=(None, None), size=(150, 50), on_release=dropdown.open)
         
-#         self.press = Button(text = 'Click me')
-#         self.press.bind(on_press = self.click_me)
-#         self.add_widget(self.press)
+        for value in values:
+            btn = Button(text=value, size_hint_y=None, height=44)
+            btn.bind(on_release=lambda btn: self.update_button_and_close_dropdown(dropdown, button, btn.text, selected_value_attr))
+            dropdown.add_widget(btn)
+
+        setattr(self, selected_value_attr, button.text)
+        return button
+
+    def create_helado_dropdown(self):
+        x = existing_df["Helado"].unique()
+        button_name = "Select Helado"
+        attribute = "selected_helado"
+        return self.create_dropdown(x, button_name, attribute)
+
+    def create_quantity_dropdown(self):
+        x = [str(number) for number in range(1, 10)]
+        button_name = "Select quantity"
+        selected_quantity = "selected_quantity"
+        return self.create_dropdown(x,  button_name, selected_quantity)
+
+    def create_operation_dropdown(self):
+        x = ["Entrada", "Salida"]
+        button_name = "Select operation"
+        selected_operation = "selected_operation"
+        return self.create_dropdown(x,  button_name, selected_operation)
+
+    def generate_entry(self, instance):
+        # Functionality to generate entry based on selected values
+        global existing_df
+        helado = self.selected_helado
+        cantidad = int(self.selected_quantity)
+        operacion = self.selected_operation
+        stock = existing_df[existing_df["Helado"] == helado]["Stock"].iloc[-1]
         
-#     def click_me(self, instance):
-#         print("Name of student is " +self.s_name.text)
-#         print("Marks of student is " +self.s_marks.text)
-#         print("Gender of student is " +self.s_gender.text)
-#         print("")
+        if operacion == "Entrada":
+            stock = stock + cantidad
+        elif operacion == "Salida":
+            stock = stock - cantidad
+            
+        message = f"Se ha hecho una {operacion.lower()} de {cantidad} unidades de {helado}"
+        self.create_new_entry(helado, cantidad, operacion, stock, message)
